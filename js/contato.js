391699eee51ab1fc19c9ee31ee3a202bf255cd62
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-25 15:24:06 -0300

    colocando lightbox em servico

--- js/contato.js
+++ js/contato.js
@@ -78,3 +78,35 @@ botaoBuscar.addEventListener("click", async function(event){
 
 
 });
+
+
+//----------------Programação do formspree----------------------------------------
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: formulario.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado pelo sua inscrição!";
+          formulario.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! Temos um problema nçao inscrição do seu formulario"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! Temos um problema nçao inscrição do seu formulario :C"
+      });
+    }
+    formulario.addEventListener("submit", handleSubmit)
